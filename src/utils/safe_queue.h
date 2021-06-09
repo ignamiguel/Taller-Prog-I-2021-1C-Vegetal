284e915ab3d6d62c9d4de3a8dd7afccbfa0216f0
cdrubio <crubio@fi.uba.ar>
2021-06-09 11:53:14 -0700

    Create safe_queue.h

---
+++ src/utils/safe_queue.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <pthread.h>
+#include <queue>
+
+template <typename T> class SafeQueue
+{
+public:
+    SafeQueue(void);
+    ~SafeQueue(void);
+    void push(T t);
+    T pop(void);
+private:
+    std::queue<T> queue;
+    pthread_mutex_t mutex;
+    pthread_cond_t condition_variable;
+};
