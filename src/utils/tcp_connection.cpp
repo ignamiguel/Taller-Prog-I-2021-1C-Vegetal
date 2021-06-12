97d3f98cd7541e310bd595689953a54e1656f6a9
cdrubio <crubio@fi.uba.ar>
2021-06-12 07:52:10 -0700

    Create tcp_connection.cpp

---
+++ src/utils/tcp_connection.cpp
@@ -0,0 +1,28 @@
+#include "tcp_connection.h"
+
+#include <unistd.h>
+#include <sys/socket.h>
+
+namespace utils
+{
+    TCPConnection::TCPConnection(int fd) : sockfd(fd)
+    {
+
+    }
+
+    TCPConnection::~TCPConnection()
+    {
+        ::close(sockfd);
+    }
+
+    ssize_t TCPConnection::send(const void *buf, size_t len)
+    {
+        return ::send(sockfd, buf, len, 0);
+    }
+
+    ssize_t TCPConnection::recv(void *buf, size_t len)
+    {
+        // Wait until there is data to read?
+        return ::recv(sockfd, buf, len, 0);
+    }
+}
