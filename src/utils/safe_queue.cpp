343fc2b887319a14a5f79343b2dc07fca995daa3
cdrubio <crubio@fi.uba.ar>
2021-06-12 07:36:06 -0700

    Add utils namespace

--- src/utils/safe_queue.cpp
+++ src/utils/safe_queue.cpp
@@ -1,39 +1,42 @@
 #include "safe_queue.h"
 
-template<class T>
-SafeQueue<T>::SafeQueue(void)
+namespace utils
 {
-    pthread_mutex_init(&m_mutex, nullptr);
-    pthread_cond_init(&m_condv, nullptr);
-}
-
-template<class T>
-SafeQueue<T>::~SafeQueue(void)
-{
-    pthread_mutex_destroy(&m_mutex);
-    pthread_cond_destroy(&m_condv);
-}
-
-template<class T>
-void SafeQueue<T>::push(T t)
-{
-    pthread_mutex_lock(&mutex);
-    queue.push(t);
-    pthread_cond_signal(&condition_variable);
-    pthread_mutex_unlock(&mutex);
-}
-
-template<class T>
-T SafeQueue<T>::pop(void)
-{
-    pthread_mutex_lock(&mutex);
-    while (queue.empty())
+    template<class T>
+    SafeQueue<T>::SafeQueue(void)
     {
-        pthread_cond_wait(&condition_variable, &mutex);
+        pthread_mutex_init(&m_mutex, nullptr);
+        pthread_cond_init(&m_condv, nullptr);
     }
-    T value = queue.front();
-    queue.pop();
-    pthread_mutex_unlock(&mutex);
-    
-    return value;
+
+    template<class T>
+    SafeQueue<T>::~SafeQueue(void)
+    {
+        pthread_mutex_destroy(&m_mutex);
+        pthread_cond_destroy(&m_condv);
+    }
+
+    template<class T>
+    void SafeQueue<T>::push(T t)
+    {
+        pthread_mutex_lock(&mutex);
+        queue.push(t);
+        pthread_cond_signal(&condition_variable);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    template<class T>
+    T SafeQueue<T>::pop(void)
+    {
+        pthread_mutex_lock(&mutex);
+        while (queue.empty())
+        {
+            pthread_cond_wait(&condition_variable, &mutex);
+        }
+        T value = queue.front();
+        queue.pop();
+        pthread_mutex_unlock(&mutex);
+        
+        return value;
+    }
 }
--- src/utils/safe_queue.h
+++ src/utils/safe_queue.h
@@ -3,15 +3,18 @@
 #include <pthread.h>
 #include <queue>
 
-template <typename T> class SafeQueue
+namespace utils
 {
-public:
-    SafeQueue(void);
-    ~SafeQueue(void);
-    void push(T t);
-    T pop(void);
-private:
-    std::queue<T> queue;
-    pthread_mutex_t mutex;
-    pthread_cond_t condition_variable;
-};
+    template <typename T> class SafeQueue
+    {
+    public:
+        SafeQueue(void);
+        ~SafeQueue(void);
+        void push(T t);
+        T pop(void);
+    private:
+        std::queue<T> queue;
+        pthread_mutex_t mutex;
+        pthread_cond_t condition_variable;
+    };
+}
